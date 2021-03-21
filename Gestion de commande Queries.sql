763b4ca1c4b963c17e30150bbd14413488e0e219
issamBoutissante <boutissante.issam.dev@gmail.com>
2021-03-21 22:27:13 +0100

    done

---
+++ Gestion de commande Queries.sql
@@ -0,0 +1,6 @@
+use GestionCom
+-- 1-	Procéder à la création de la base de données GestionCom
+--       Deja Cree
+
+-- 2-	Ecrire un programme qui calcule le montant de la commande numéro 10 et affiche un message 
+-- 'Commande Normale' ou 'Commande Spéciale' selon que le montant est inférieur ou supérieur à 100000 DH 
